67efbf668a11a03e0f0533b81d8455d09a51c3b6
Sagor <mbrsagor@gmail.com>
2020-11-02 00:27:08 +0600

    Hook button onClick

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -6,9 +6,17 @@ function Hooks() {
         full_name: "Md.Bozlur Rosid Sagor"
     })
 
+    const changeHandler = () => {
+        setName({
+            full_name: 'Mbr-Sagor'
+        });
+    }
+
     return (
         <div>
             <h2>{name.full_name}</h2>
+            {/* <button onClick={() => setName({full_name:'Mbr-Sagor'})}>Change Name</button> */}
+            <button onClick={changeHandler}>Change Name</button>
         </div>
     )
 }
