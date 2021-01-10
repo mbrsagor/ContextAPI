3d80ae439a88b32655081586c90137f4210bef7a
Sagor <mbrsagor@gmail.com>
2021-01-10 23:30:36 +0600

    Class base App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Component } from "react";
 import Counter from './components/counter';
 import { CounterCountext } from "./context/counter_context";
 
-function App() {
-  return (
-    <div className="App">
-      <CounterCountext>
-        <Counter />
-      </CounterCountext>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CounterCountext>
+          <Counter />
+        </CounterCountext>
+      </div>
+    );
+  }
 }
 
 export default App;
