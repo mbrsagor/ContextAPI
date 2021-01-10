f09dca1852075c16f3075bd17c4beb833a0a44a9
Sagor <mbrsagor@gmail.com>
2021-01-10 22:06:09 +0600

    Refactor

--- src/api/car.js
+++
@@ -1,6 +0,0 @@
-const car = [
-    {name: 'sagor', age: 26, address: 'Dhaka-Bangladesh'},
-    {name: 'sagor', age: 26, address: 'Dhaka-Bangladesh'},
-    {name: 'sagor', age: 26, address: 'Dhaka-Bangladesh'},
-    {name: 'sagor', age: 26, address: 'Dhaka-Bangladesh'}
-]
