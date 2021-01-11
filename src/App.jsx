51835ed6edc96f24cffbd0150269dbb265d2ee79
Sagor <mbrsagor@gmail.com>
2021-01-11 16:07:11 +0600

    Clean project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Counter context</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
